f8f30fa375fe46bd40f3ce7ea5eaa629f6cdc325
Risa Fujii <risaf1231@gmail.com>
2018-11-11 11:32:55 +0000

    Edit models, have not migrated db yet

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,7 @@
 class Member < ApplicationRecord
-  belongs_to :favorite_game
-  belongs_to :available_days
+  validates :name, :favorite_game, presence: true
+  validates :name, length: {minimum: 2, maximum: 8}, uniqueness: true
+  
+  belongs_to :favorite_game, foreign_key: "name", class_name: “Game”
+  belongs_to :available_days, foreign_key: "name", class_name: “WeekDay”
 end
