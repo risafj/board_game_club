8dbfc4e8115f79d524741c22bd732b5ef7ecc7a3
Risa Fujii <risaf1231@gmail.com>
2018-12-15 21:55:58 +0900

    Disable Rubocop's warning about habtm so I don't have to change the relationship

--- db/migrate/20181117041813_create_join_table_member_available_days.rb
+++ db/migrate/20181117041813_create_join_table_member_available_days.rb
@@ -1,6 +1,7 @@
 class CreateJoinTableMemberAvailableDays < ActiveRecord::Migration[5.2]
   def change
     create_join_table :members, :weekdays do |t|
+      # %i[] means an array of symbols.
       t.index %i[member_id weekday_id]
       t.index %i[weekday_id member_id]
     end
